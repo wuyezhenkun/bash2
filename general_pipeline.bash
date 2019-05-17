ea2e8c03ae709a95787125e8e96dbaf84964b36a
WeiliangBMBL <liu.3540@osu.edu>
2019-05-17 05:37:19 -0400

    Update and rename general_pipeline to general_pipeline.bash

---
+++ general_pipeline.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p PM --mem=128GB
+#SBATCH --ntasks-per-node 28
+#SBATCH -t 24:00:00
+# echo commands to stdout 
+set –x
+
+
+## This bash file tests all commands in the general pipeline
+
+
+cd /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/test1
+
+## 3.1 Part 1 - Gene mapping and catalog construction
+
+./master/runmeta --mapping -s ./meta_data/data/samples/ -ref ./meta_data/data/ref/IGC_ref.fa -o1 ./meta_data/results/align_out/ -o2 ./meta_data/results/cat/
+
+## 3.2 Part 2 - hGEM generation and filtering
+## Part 2.1
+
+./master/runmeta --CatToMat -i ./meta_data/results/cat/ -m ./meta_data/data/735_hGEM/ -o ./meta_data/results/hGEM/
+
+## Part 2.2
+
+./master/runmeta --filtering -i ./meta_data/results/hGEM/ -minTPM 1 -NominTPM 2 -o ./meta_data/results/hGEM_filt/
+
+## 3.3 Part 3 - Biclustering and enrichment analyses
+## Part 3.1
+
+./master/runmeta --biclustering -i ./meta_data/results/hGEM_filt/ -o ./meta_data/results/bic/ --QUBIC -c 1 -f 0.8 -o 100 
+
+## Part 3.2
+
+./master/runmeta --enrichment -i ./meta_data/results/bic -g ./meta_data/data/annotation/KEGG_ann.txt -o ./meta_data/results/enrich_out/
