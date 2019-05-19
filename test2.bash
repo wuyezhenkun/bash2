24ca268d7cbb17660c658dc6be2efc6140bdc62e
WeiliangBMBL <liu.3540@osu.edu>
2019-05-18 20:54:47 -0400

    Update test2.bash

--- test2.bash
+++ test2.bash
@@ -23,7 +23,7 @@ cd /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/test2
 
 ## Part 2.2
 
-./master/runmeta --filtering -i ./meta_data/results/hGEM/ -minTPM 1 -NominTPM 2 -o ./meta_data/results/hGEM_filt/
+./master/runmeta --filtering -i ./meta_data/data/ref/IGC_ref.fa -m ./meta_data/results/hGEM/  -minTPM 1 -NominTPM 2 -o ./meta_data/results/hGEM_filt/
 
 ## 3.3 Part 3 - Biclustering and enrichment analyses
 ## Part 3.1
