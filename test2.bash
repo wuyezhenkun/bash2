72681a3181b4280ee8dae899478bb30c301543c3
WeiliangBMBL <liu.3540@osu.edu>
2019-05-24 12:37:01 -0400

    Update test2.bash

--- test2.bash
+++ test2.bash
@@ -14,22 +14,22 @@ cd /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/test2
 
 ## 3.1 Part 1 - Gene mapping and catalog construction
 
-./master/runmeta --mapping -s ./meta_data/data/samples/ -ref ./meta_data/data/ref/IGC_ref.fa -o1 ./meta_data/results/align_out/ -o2 ./meta_data/results/cat/
+./metaqubic-master/runmeta --mapping -s ./meta_data/data/samples/ -ref ./meta_data/data/ref/IGC_ref.fa -o1 ./meta_data/results/align_out/ -o2 ./meta_data/results/cat/
 
 ## 3.2 Part 2 - hGEM generation and filtering
 ## Part 2.1 (4.2)
 
-./master/runmeta --CatToMat -i ./meta_data/results/cat/ -o ./meta_data/results/hGEM/
+./metaqubic-master/runmeta --CatToMat -i ./meta_data/results/cat/ -o ./meta_data/results/hGEM/
 
 ## Part 2.2
 
-./master/runmeta --filtering -i ./meta_data/data/ref/IGC_ref.fa -m ./meta_data/results/hGEM/  -minTPM 1 -NominTPM 2 -o ./meta_data/results/hGEM_filt/
+./metaqubic-master/runmeta --filtering -i ./meta_data/data/ref/IGC_ref.fa -m ./meta_data/results/hGEM/  -minTPM 1 -NominTPM 2 -o ./meta_data/results/hGEM_filt/
 
 ## 3.3 Part 3 - Biclustering and enrichment analyses
 ## Part 3.1
 
-./master/runmeta --biclustering -i ./meta_data/results/hGEM_filt/RDRPK_hGEM_filt.txt -o ./meta_data/results/bic/ --QUBIC -c 1 -f 0.8 -o 100 
+./metaqubic-master/runmeta --biclustering -i ./meta_data/results/hGEM_filt/RDRPK_hGEM_filt.txt -o ./meta_data/results/bic/ --QUBIC -c 1 -f 0.8 -o 100 
 
 ## Part 3.2
 
-./master/runmeta --enrichment -i ./meta_data/results/bic/RDRPK_735_hGEM.txt.blocks -g ./meta_data/data/annotation/KEGG_ann.txt -o ./meta_data/results/enrich_out/
+./metaqubic-master/runmeta --enrichment -i ./meta_data/results/bic/RDRPK_735_hGEM.txt.blocks -g ./meta_data/data/annotation/KEGG_ann.txt -o ./meta_data/results/enrich_out/
