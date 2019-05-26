f6d0289fec28b2fef0b94542a74074feb00daaca
WeiliangBMBL <liu.3540@osu.edu>
2019-05-26 00:07:08 -0400

    Update Step3.bash

--- Step3.bash
+++ Step3.bash
@@ -7,12 +7,16 @@
 set –x
 
 ## get to the directory
-cd /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/test3
+cd /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/
 
 
 
-## biclustering OK
+./metaqubic-master/runmeta --mapping -s ./meta_data/data/samples/ -ref ./meta_data/data/ref/IGC_ref.fa -o1 ./meta_data/results/align_out/ -o2 ./meta_data/results/cat/
+
+./metaqubic-master/runmeta --CatToMat -i ./meta_data/results/cat/ -m ./meta_data/data/735_hGEM/ -o ./meta_data/results/hGEM/
+
+./metaqubic-master/runmeta --filtering -i ./meta_data/data/ref/IGC_ref.fa -m ./meta_data/results/hGEM/ -minTPM 1 -NominTPM 2 -o ./meta_data/results/hGEM_filt/
+
 ./metaqubic-master/runmeta --biclustering -i ./meta_data/results/hGEM_filt/RDRPK_hGEM_filt.txt -o ./meta_data/results/bic/ --QUBIC -f 0.8 -o 200 
 
-## enrichment OK
-./metaqubic-master/runmeta --enrichment -i ./meta_data/results/bic/RDRPK_hGEM_filt.txt.blocks -g ./meta_data/data/annotations/kegg_annotation.txt -o ./meta_data/results/enrich_out/
+./metaqubic-master/runmeta --enrichment -i ./meta_data/results/bic/RDRPK_735_hGEM.txt.blocks -g ./meta_data/data/annotation/KEGG_annotation.txt -o ./meta_data/results/enrich_out/
