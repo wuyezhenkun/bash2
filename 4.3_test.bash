344d55a3d19dc6f0896e97548b627b499e3c6267
WeiliangBMBL <liu.3540@osu.edu>
2019-05-28 18:26:01 -0400

    Create 4.3_test.bash

---
+++ 4.3_test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p PM --mem=512GB
+#SBATCH --ntasks-per-node 28
+#SBATCH -t 24:00:00
+# echo commands to stdout 
+set –x
+
+cd /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/4.3_test
+
+
+
+./metaqubic-master/runmeta --biclustering -i ./meta_data/results/hGEM_filt/RDRPK_hGEM_filt.txt -o ./meta_data/results/bic/ --QUBIC -f 0.8 -o 200 
+
+./metaqubic-master/runmeta --enrichment -i ./meta_data/results/bic/RDRPK_hGEM_filt.txt.blocks -g ./meta_data/data/annotation/KEGG_annotation.txt -o ./meta_data/results/enrich_out/
